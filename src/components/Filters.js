5719780311b7cd1a56a429a195065542eb46aa23
Hanan88 <Hanan.Ahmed.8@hotmail.com>
2022-10-27 01:10:58 +0200

    Add: Filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -2,7 +2,35 @@ import React from "react";
 import "./Filters.css";
 
 const Filters = () => {
-  return <div>Filters</div>;
+  return (
+    <div className="filters__controls">
+      <div className="filters__control">
+        <label>Employee Name</label>
+        <input type="text" placeholder="e.g Admin.User"/>
+      </div>
+      <div className="filters__control">
+        <label>Action Type</label>
+        <input type="text" />
+      </div>
+      <div className="filters__control">
+        <label>Application Type</label>
+        <input type="text" />
+      </div>
+      <div className="filters__control">
+        <label>From Date</label>
+        <input type="date" />
+      </div>
+      <div className="filters__control">
+        <label>To Date</label>
+        <input type="date" />
+      </div>
+      <div className="filters__control">
+        <label>Application ID</label>
+        <input type="text" />
+      </div>
+      <button>Search Logger</button>
+    </div>
+  );
 };
 
 export default Filters;
