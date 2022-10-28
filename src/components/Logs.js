cc9052e44a4cd0bbce19c72726c9dd71cd287c73
Hanan88 <Hanan.Ahmed.8@hotmail.com>
2022-10-28 02:08:34 +0200

    Add: Sorting

--- src/components/Logs.js
+++ src/components/Logs.js
@@ -5,17 +5,35 @@ import ReactPaginate from "react-paginate";
 import axios from "axios";
 
 const Logs = () => {
+  // const [logResponse, setLogResponse] = useState([]);
   const [logData, setLogData] = useState([]);
   const [pageNumber, setPageNumber] = useState(0);
   const [itemsPerPage, setItemsPerPage] = useState(10);
   const pagesVisited = pageNumber * itemsPerPage;
-  const pageCount = Math.ceil(logData?.auditLog?.length / itemsPerPage);
+  const pageCount = Math.ceil(logData?.length / itemsPerPage);
+  const [order, setOrder] = useState("ASC");
+
+  const getLogData = async () => {
+    try {
+      const response = await axios.get(
+        "https://run.mocky.io/v3/a2fbc23e-069e-4ba5-954c-cd910986f40f"
+      );
+      // setLogResponse(response.data.result);
+      setLogData(response.data.result.auditLog);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getLogData();
+  }, []);
 
   const changePages = ({ selected }) => {
     setPageNumber(selected);
   };
 
-  const displayLogs = logData?.auditLog
+  const displayLogs = logData
     ?.slice(pagesVisited, pagesVisited + itemsPerPage)
     .map((log) => {
       return (
@@ -30,32 +48,69 @@ const Logs = () => {
       );
     });
 
-  const getLogData = async () => {
-    try {
-      const response = await axios.get(
-        "https://run.mocky.io/v3/a2fbc23e-069e-4ba5-954c-cd910986f40f"
-      );
-      setLogData(response.data.result);
-    } catch (error) {
-      console.log(error);
+  const sorting = (col) => {
+    console.log(col, "1");
+    if (order === "ASC") {
+      const sorted = [...logData].sort((a, b) => (a[col] > b[col] ? 1 : -1));
+      setLogData(sorted);
+      setOrder("DSC");
+    }
+
+    if (order === "DSC") {
+      const sorted = [...logData].sort((a, b) => (a[col] < b[col] ? 1 : -1));
+      setLogData(sorted);
+      setOrder("ASC");
     }
   };
-
-  useEffect(() => {
-    getLogData();
-  }, []);
 
   return (
     <div className="logs">
       <table className="logs__table">
         <thead className="logs__table__head">
           <tr>
-            <th>Log ID</th>
-            <th>Application Type</th>
-            <th>Application ID</th>
-            <th>Action</th>
+            <th>
+              Log ID
+              <button className="sort_btn" onClick={() => sorting("logId")}>
+                &uarr;
+              </button>
+            </th>
+            <th>
+              Application Type
+              <button
+                className="sort_btn"
+                onClick={() => sorting("applicationType")}
+              >
+                &uarr;
+              </button>
+            </th>
+            <th>
+              Application ID
+              <button
+                className="sort_btn"
+                onClick={() => sorting("applicationId")}
+              >
+                &uarr;
+              </button>
+            </th>
+            <th>
+              Action
+              <button
+                className="sort_btn"
+                onClick={() => sorting("actionType")}
+              >
+                &uarr;
+              </button>
+            </th>
             <th>Action Details</th>
-            <th>Date : Time</th>
+            <th>
+              Date : Time
+              <button
+                className="sort_btn"
+                onClick={() => sorting("creationTimestamp")}
+              >
+                &uarr;
+              </button>
+            </th>
           </tr>
         </thead>
         <tbody className="logs__table__body">{displayLogs}</tbody>
